b51ea8688ea771873b6a83ec635dfe7517b805cb
Kalavrat <97897883+Kalavrat@users.noreply.github.com>
2022-02-08 11:37:54 +0300

    Add files via upload

---
+++ samostoyalka.js
@@ -0,0 +1,36 @@
+// sum = 55000
+// kolvo = 10
+// promo = null
+// function calculate(sum, kolvo, promo)
+// {
+//     if (promo == "ДАРИМ300")
+//     {
+//         sum = sum - 300
+//         if (sum < 0 )
+//         {
+//             sum = 0
+//         }
+//     }
+//     console.log(sum)
+//     if (kolvo >= 10)
+//     {
+//         sum = sum * 0.95 
+//     }
+//     console.log(sum)
+//     if (sum > 50000)
+//     {
+//         let raznica = sum - 50000
+//         raznica = raznica * 0.2
+//         sum = sum - raznica
+//     }
+//     console.log(sum)
+//     if (promo = "СКИДКА15")
+//     {
+//         if (sum >= 20000)
+//         {
+//             sum = sum * 0.85
+//         }
+//     }
+//     console.log(sum)
+// }
+// calculate(sum, kolvo, promo)
